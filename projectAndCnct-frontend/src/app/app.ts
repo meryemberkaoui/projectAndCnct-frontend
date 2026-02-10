c2aa4ddcf5ce0650607b7ff461b5910e9309ac7d
Meryem Berkaoui <meryem.berkaoui20@meryem.com>
2026-02-10 20:49:12 +0100

    Compoment header

--- projectAndCnct-frontend/src/app/app.ts
+++ projectAndCnct-frontend/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('projectAndCnct-frontend');
+  openAuthModal() {
+    console.log('Ouvre modal Auth');
+  }
+
 }
